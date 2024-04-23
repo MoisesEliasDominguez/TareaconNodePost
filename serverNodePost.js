a503a2d65341e00d8a341231255c4d1d5b2f1945
MoisesEliasDominguez <166049348+MoisesEliasDominguez@users.noreply.github.com>
2024-04-23 11:40:56 -0500

    serverNodePost.js

--- serverNodePost.js
+++ serverNodePost.js
@@ -9,20 +9,16 @@ const server = servidor.createServer((req, res) => {
     if (req.method === 'POST') {
         let body = '';
 
-        // Manejano de los datos de entrada 
         req.on('data', (chunk) => {
             body += chunk;
         });
 
-        // Cuando se han recibido todos los datos, procesamos el formulario 
         req.on('end', () => {
-            // Parseamos los datos del formulario
+
             const formData = new URLSearchParams(body.toString());
-            // Obtenemos los valores de username y contraseña
             const userName = formData.get('username');
             const password = formData.get('password');
             
-            // Enviamos una respuesta con los datos recibidos
             res.setHeader('Content-Type', 'text/html');
             res.write(`<p>Username: ${userName}</p>`);
             res.write(`<p>Password: ${password}</p>`);
@@ -31,7 +27,6 @@ const server = servidor.createServer((req, res) => {
     }
     
     else {
-        // Si la solicitud no es un POST, enviamos el formulario
         res.setHeader('Content-Type', 'text/html');
         res.write('<form method="POST">');
         res.write('<label for="username">Username:</label>');
@@ -44,6 +39,5 @@ const server = servidor.createServer((req, res) => {
     }
 });
 
-server.listen(5001, () => {
-    console.log('Servidor escuchando en el puerto 5001');
+server.listen(5001);
 });
